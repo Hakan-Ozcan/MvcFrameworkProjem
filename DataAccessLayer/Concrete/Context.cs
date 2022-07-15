795b5acbef2ec69528a5cdac7b2fa827d7be4957
furkan <furkan@DESKTOP-1NRATPK>
2022-07-15 19:23:00 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        public Context() : base(@"Server=DESKTOP-1NRATPK\SQLEXPRESS;Database=MvcFrameworkProjem;Trusted_Connection=True;")
+        {
+
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
---
+++ MvcFrameworkProjem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcFrameworkProjem.MvcApplication" Language="C#" %>
