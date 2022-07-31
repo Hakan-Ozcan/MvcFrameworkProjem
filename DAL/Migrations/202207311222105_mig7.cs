bdd27889f3fe18342f34c894ada2ce3b46bb6fc0
Hakan Özcan <hakanse12@hotmail.com>
2022-07-31 21:41:01 +0300

    Writer kısmının admin paneli bitirildi

---
+++ DAL/Migrations/202207311222105_mig7.cs
@@ -0,0 +1,34 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig7 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Writers", "WriterAbout", c => c.String(maxLength: 100));
+            AlterColumn("dbo.Writers", "WriterMail", c => c.String(maxLength: 200));
+            AlterColumn("dbo.Writers", "WriterPassword", c => c.String(maxLength: 200));
+
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Headings", "WriterID", "dbo.Writers");
+            DropForeignKey("dbo.Contents", "WriterID", "dbo.Writers");
+            DropForeignKey("dbo.Contents", "HeadingID", "dbo.Headings");
+            DropForeignKey("dbo.Headings", "CategoryID", "dbo.Categories");
+            DropIndex("dbo.Contents", new[] { "WriterID" });
+            DropIndex("dbo.Contents", new[] { "HeadingID" });
+            DropIndex("dbo.Headings", new[] { "WriterID" });
+            DropIndex("dbo.Headings", new[] { "CategoryID" });
+            DropTable("dbo.Contacts");
+            DropTable("dbo.Writers");
+            DropTable("dbo.Contents");
+            DropTable("dbo.Headings");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Abouts");
+        }
+    }
+}
