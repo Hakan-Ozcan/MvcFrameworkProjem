cc7e216e910ddabdfcc2c09ea4ac9e0f25270908
Hakan Özcan <hakanse12@hotmail.com>
2022-08-15 21:57:37 +0300

    mesaj bölümü kısımları eklendi

---
+++ MvcFrameworkProjem/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using BLL.Concrete;
+using BLL.ValidationRules;
+using DAL.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcFrameworkProjem.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactDal());
+        ContactValidator cv = new ContactValidator();
+        public ActionResult Index()
+        {
+            var contactvalues = cm.GetList();
+            return View(contactvalues);
+        }
+        public ActionResult GetContactDetails(int id)
+        {
+            var contactvalues= cm.GetByID(id);
+            return View(contactvalues);
+        }
+    }
+}
